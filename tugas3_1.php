aa816f3bac65f06e194dbf3315cb1b728ce19ef4
troy213 <49593922+troy213@users.noreply.github.com>
2021-01-18 01:25:31 +0700

    Add files via upload

---
+++ tugas3_1.php
@@ -0,0 +1,10 @@
+<?php
+  $jarak = 360;
+  $v = 90;
+  $t = $jarak / $v * 60;
+
+  echo "Jarak kota A - Z = $jarak Km <br>
+  Kecepatan motor = $v <br>
+  Waktu tempuh = ? <br>
+  Jawaban : $t Menit";
+?>
---
+++ tugas3_2.php
@@ -0,0 +1,10 @@
+<?php
+  $awal = 150000;
+  $bunga = 12.5;
+  $total = $awal + ( $awal * ( $bunga / 100 ));
+
+  echo "Tabungan Awal = Rp $awal <br>
+  Bungan Setahun = $bunga % <br>
+  Total Tabungan Setahun = ? <br>
+  Jawaban : Rp $total;"
+?>
